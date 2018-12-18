baf4f0dd45afd4a9fa884ba3f6d6d11fbb41dc88
ruosuburan <44898154+ruosuburan@users.noreply.github.com>
2018-12-18 14:39:59 +0800

    From F where saves myproject
    The global variable

---
+++ global.h
@@ -0,0 +1,17 @@
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                            global.h
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                                    FS */
+
+
+EXTERN	struct file_desc	f_desc_table[NR_FILE_DESC];
+EXTERN	struct inode		inode_table[NR_INODE];
+EXTERN	struct super_block	super_block[NR_SUPER_BLOCK];
+extern	u8 *			fsbuf;
+extern	const int		FSBUF_SIZE;
+EXTERN	MESSAGE			fs_msg;
+EXTERN	struct proc *		pcaller;
+EXTERN	struct inode *		root_inode;
+extern	struct dev_drv_map	dd_map[];
+
